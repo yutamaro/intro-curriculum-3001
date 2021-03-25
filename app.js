8865f6bcd177ef1504491087b3596d75e63f12c3
Kaito Sugimoto <kaito_sugimoto@nii.ac.jp>
2021-03-25 16:30:31 +0900

    fix indent

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 * @returns {Number}
 */
 function factorial(n) {
-    let result = 1;
-    // TODO このコメントを消して正しく実装してください。
-    return result;
+  let result = 1;
+  // TODO このコメントを消して正しく実装してください。
+  return result;
 }
 const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
