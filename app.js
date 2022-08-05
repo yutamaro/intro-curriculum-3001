c79d92bb9f49690eb501b0b2f7355328854f8728
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2022-08-05 15:14:40 +0900

    JSDoc のインデントを修正

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 /**
-* 与えられた自然数の階乗を返す
-* 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
-* @param {Number} n
-* @returns {Number}
-*/
+ * 与えられた自然数の階乗を返す
+ * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
+ * @param {Number} n
+ * @returns {Number}
+ */
 function factorial(n) {
   let result = 1;
   // TODO このコメントを消して正しく実装してください。
